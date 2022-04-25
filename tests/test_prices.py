4c6eab6b69541b00425985aeb73320063d6ef680
rui sp <manuelsapereira@gmail.com>
2022-04-25 16:37:24 +0800

    Added first test.

---
+++ tests/test_prices.py
@@ -0,0 +1,7 @@
+from fmp_extractor.prices.historic import extract_prices_high_frequency
+
+
+def test_high_freq():
+    symbol = 'TWTR'
+    df = extract_prices_high_frequency('TWTR', '30min').set_index('date').close.to_frame(name=f'{symbol}').asfreq('h')
+    assert symbol in df.columns
