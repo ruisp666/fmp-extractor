4909705deffff016b30c89e75389cc86e523f05a
rui sp <manuelsapereira@gmail.com>
2022-05-19 16:32:25 +0800

    Added function to extract the latest snapshot of all nyse stocks

--- src/fmp_extractor/prices/live.py
+++ src/fmp_extractor/prices/live.py
@@ -25,29 +25,60 @@ def extract_prices_live(tickers):
     return df
 
 
-def extract_prices_batch(tickers: list, api_key: str = API_KEY):
+def extract_prices_batch(tickers: list):
     """
     Extracts batch quotes.
     Parameters
     ----------
     tickers: list
         List of tickers
-    api_key: str
-        They Api Key
     Returns
     -------
     batch_prices_df: pd.DataFrame
     """
     str_tickers_ = ','.join(tickers)
-    url = f'{API_URL}v3/quote/{str_tickers_}?apikey={api_key}'
+    url = f'{API_URL}v3/quote/{str_tickers_}?apikey={API_KEY}'
     full_info = get_jsonparsed_data(url)
     full_info_df = pd.DataFrame.from_records(full_info)
     full_info_df['timestamp'] = pd.to_datetime(full_info_df['timestamp'], unit='s')
     return full_info_df
 
 
-if __name__ == '__main__':
-    str_tickers = ['TWTR', 'NVDA']
-    turn = extract_prices_batch(str_tickers)
-    print(turn.columns)
-    print(turn.head(5))
+def extract_prices_all_nyse():
+    """
+    Extracts latest snapshot of all stocks traded in nyse.
+    --------------------
+    Example
+       {
+      "symbol" : "GSL",
+      "name" : "Global Ship Lease, Inc.",
+      "price" : 23.73000000,
+      "changesPercentage" : 5.41981030,
+      "change" : 1.21999930,
+      "dayLow" : 22.50000000,
+      "dayHigh" : 23.82000000,
+      "yearHigh" : 30.02000000,
+      "yearLow" : 14.33000000,
+      "marketCap" : 875907520.00000000,
+      "priceAvg50" : 26.21860000,
+      "priceAvg200" : 23.45430000,
+      "volume" : 925819,
+      "avgVolume" : 1288331,
+      "exchange" : "NYSE",
+      "open" : 22.50000000,
+      "previousClose" : 22.51000000,
+      "eps" : 4.60000000,
+      "pe" : 5.15869570,
+      "earningsAnnouncement" : "2022-05-09T12:30:00.000+0000",
+      "sharesOutstanding" : 36911400,
+      "timestamp" : 1651648804
+    }
+    """
+    url = f'{API_URL}v3/quotes/nyse?apikey={API_KEY}'
+    full_info = get_jsonparsed_data(url)
+    full_info_df = pd.DataFrame.from_records(full_info)
+    full_info_df['timestamp'] = pd.to_datetime(full_info_df['timestamp'], unit='s')
+    full_info_df['earningsAnnouncement'] = pd.to_datetime(full_info_df['earningsAnnouncement'], format="%Y-%m-%dT%H:%M")
+    return full_info_df
+
+
